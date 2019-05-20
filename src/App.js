45c5cfe8a3f9d9b7085e3974bcae45ef869d3a97
hashy0917 <oohasi3939@gmail.com>
2019-05-20 12:03:51 +0900

    add under clock

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Clock from './Clock'
+
 function App() {
   return (
     <div className="App">
@@ -19,6 +21,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Clock />
     </div>
   );
 }
